207b139befda193d8a9b8ffba17a62b78d9cc50a
GoIah <100486782+GoIah@users.noreply.github.com>
2023-09-14 19:11:18 +0000

    made changes to repo lol.

---
+++ docs/main.js
@@ -0,0 +1,6 @@
+function col() {
+    document.getElementById("ps").innerHTML = "caca";
+    document.getElementById("pps").innerHTML = "caca";
+    document.getElementById("Coolimg").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUIOH7nVft1ZON-9XoVsfDooi6hIjdsEsG4tnPobzrxnJ5FmrIGB93gy2CDOqVFrpSQkQ:https://cdn.cloudflare.steamstatic.com/steam/apps/236390/ss_aef518de9c6987834e6302075e712911663669a2.1920x1080.jpg%3Ft%3D1689771864&usqp=CAU";
+};
+document.getElementById("ps").innerHTML = "caca";
